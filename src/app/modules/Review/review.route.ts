52b38f0f3fe35f7c2ec8ad55d4e4be2e49e438c7
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-05-09 10:14:35 +0600

    Review sent successfully!

--- src/app/modules/Review/review.route.ts
+++ src/app/modules/Review/review.route.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import { ReviewsController } from './review.controller';
 import auth from '../../middlewares/auth';
 import { UserRole } from '@prisma/client';
+import { validateRequest } from '../../middlewares/validateRequest';
+import { ReviewsValidation } from './review.validation';
 
 const router = express.Router();
 
 router.post(
   '/',
   auth(UserRole.USER),
+  validateRequest(ReviewsValidation.createReviewSchema),
   ReviewsController.sendReview
 );
 
